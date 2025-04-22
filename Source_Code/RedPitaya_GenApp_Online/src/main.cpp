95cebf41868e669df50af6d187897bcd97d43f9e
ha210234 <aymane.hajjaoui@etu.unice.fr>
2025-04-22 08:21:37 +0200

    v1.2

--- Source_Code/RedPitaya_GenApp_Online/src/main.cpp
+++ Source_Code/RedPitaya_GenApp_Online/src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.toleconfig");
+    auto app = Gtk::Application::create(argc, argv, "org.gtkmm.rp-gen");
     Vue vue;
     return app->run(vue);
 }
